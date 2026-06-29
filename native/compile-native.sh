c1290747719a57ac6ea7f8174c010dbac363ce34
BuffOvrFlw <ffrick0001980@proton.me>
2026-06-29 15:17:50 +0000

    Merge both native codes

--- native/compile-native.sh
+++ native/compile-native.sh
@@ -9,12 +9,10 @@ C_DIR=$CWD/src/main/c
 LIBDEFLATE=$CWD/libdeflate
 MBEDTLS=$CWD/mbedtls
 RESOURCES=$CWD/src/main/resources
-NATIVE_COMPRESS=$C_DIR/NativeCompress
-NATIVE_CIPHER=$C_DIR/NativeCipher
 JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
 
 echo "COMPILE: mbedtls"
-(cd mbedtls && CFLAGS="-fPIC -I$NATIVE_CIPHER/src -DMBEDTLS_USER_CONFIG_FILE='<mbedtls_custom_config.h>'" $MAKE no_test)
+(cd mbedtls && CFLAGS="-fPIC -I$C_DIR/src -DMBEDTLS_USER_CONFIG_FILE='<mbedtls_custom_config.h>'" $MAKE no_test)
 echo "DONE"
 
 echo "COMPILE: libdeflate"
@@ -23,12 +21,8 @@ echo "DONE"
 # echo "Compiling zlib"
 # (cd zlib && CFLAGS="-fPIC -DNO_GZIP" ./configure --static && make -j$NPROC)
 
-echo "COMPILE: NativeCompress"
-(cd $NATIVE_COMPRESS && cmake -D JAVA_HOME=$JAVA_HOME -D LIBDEFLATE=$LIBDEFLATE . && $MAKE && mv $NATIVE_COMPRESS/native-compress.so $RESOURCES)
-echo "DONE"
-
-echo "COMPILE: NativeCipher"
-(cd $NATIVE_CIPHER && cmake -D JAVA_HOME=$JAVA_HOME -D MBEDTLS=$MBEDTLS . && $MAKE && mv $NATIVE_CIPHER/native-cipher.so $RESOURCES)
+echo "COMPILE: BungeeNative"
+(cd $C_DIR && cmake -D JAVA_HOME=$JAVA_HOME -D LIBDEFLATE=$LIBDEFLATE -D MBEDTLS=$MBEDTLS . && $MAKE && mv $C_DIR/native-compress.so $RESOURCES && mv $C_DIR/native-cipher.so $RESOURCES)
 echo "DONE"
 
 # $CC $CFLAGS -Izlib -o NativeCompressImpl.o src/main/c/NativeCompressImpl.c
