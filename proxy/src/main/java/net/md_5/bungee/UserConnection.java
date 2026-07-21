3c68a1b571f4ca5755396bf7da1a73fc0124d651
BuffOvrFlw <ffrick0001980@proton.me>
2026-07-21 15:39:11 -0700

    debug build

--- proxy/src/main/java/net/md_5/bungee/UserConnection.java
+++ proxy/src/main/java/net/md_5/bungee/UserConnection.java
@@ -347,9 +347,10 @@ public final class UserConnection implements ProxiedPlayer {
             if (!future.isSuccess()) {
                 future.channel().close();
                 pendingConnects.remove(target);
-
+                
+                System.out.println(future.cause());
                 final ServerInfo def = updateAndGetNextServer(target);
-                if (BungeeCord.getInstance().getConfig().getFallback() && request.isRetry() && def != null && (getServer() == null || !def.equals(getServer().getInfo()))) {
+                if (request.isRetry() && def != null && (getServer() == null || !def.equals(getServer().getInfo()))) {
                     connect(def, (result, error) -> {
                         if (result && request.isSendFeedback()) {
                             sendMessage(bungee.getTranslation("fallback_lobby"));
